d19b66d5c7869993231b61ba41988f53bc48db8a
KohaJeseMen <kohajesemen@yahoo.com>
2018-04-03 21:56:57 +0700

    Update QLLinhKien.sql

--- QLLinhKien/QLLinhKien.sql
+++ QLLinhKien/QLLinhKien.sql
@@ -1,4 +1,4 @@
-﻿/*===================================================================================/
+/*===================================================================================/
 	TẬP LỆNH TRUY VẤN TẠO CẤU TRÚC VÀ THAO TÁC CƠ SỞ DỮ LIỆU
 	Những người thực hiện: Nguyễn Hoàng Anh Khoa, Trần Ngọc Lượng
 	Đơn vị: Lớp DH17PM - Đại học An Giang
@@ -15,9 +15,9 @@ GO
 
 -- Tạo cơ sở dữ liệu QLLinhKien --
 CREATE DATABASE [QLLinhKien] ON
-( NAME = N'QLLinhKien', FILENAME = N'Z:\QLLinhKien\QLLinhKien.mdf' , SIZE = 2048KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+( NAME = N'QLLinhKien', FILENAME = N'Z:\Private\Database\QLLinhKien.mdf' , SIZE = 3136KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
  LOG ON 
-( NAME = N'QLLinhKien_log', FILENAME = N'Z:\QLLinhKien\QLLinhKien_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 1024KB )
+( NAME = N'QLLinhKien_log', FILENAME = N'Z:\Private\Database\QLLinhKien_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 1024KB )
 GO
 
 -- Sử dụng cơ sở dữ liệu QLLinhKien để thao tác --
@@ -27,96 +27,142 @@ GO
 /*-----------------------------------------------------------------------------------/
 	PHẦN TẠO LẬP CẤU TRÚC CÁC BẢNG VÀ KHÓA RÀNG BUỘC DỮ LIỆU QUAN HỆ
 /-----------------------------------------------------------------------------------*/
--- Tạo cấu trúc bảng tblCPU --
-CREATE TABLE [tblCPU](
-	[id] [char](10) NOT NULL,
-	[ten] [nvarchar](50) NOT NULL,
-	[hang] [nvarchar](50) NOT NULL,
-	[loai] [nchar](10) NOT NULL,
-	[thehe] [nchar](10) NOT NULL,
-	[socket] [nchar](10) NOT NULL,
-	[mau] [nchar](10) NOT NULL,
-	[socore] [nchar](10) NOT NULL,
-	[sothread] [nchar](10) NOT NULL,
-	[xungnhip] [nchar](10) NOT NULL,
-	[kichthuocbandan] [nchar](10) NOT NULL,
-	[tannhiet] [nchar](10) NOT NULL,
-	[dientieuthu] [nchar](10) NOT NULL,
-	[gia] [int] NOT NULL,
-	[soluong] [int] NOT NULL
+CREATE TABLE [Users]
+(
+	[id] [nchar](10) NOT NULL,
+	[username][nvarchar](50) NOT NULL,
+	[password][nvarchar](50) NOT NULL,
+	[email][nvarchar](50) NOT NULL,
+	CONSTRAINT [PK_Users] PRIMARY KEY ([id],[username])
 )
 GO
 
--- Tạo cấu trúc bảng tblOCung --
-CREATE TABLE [tblOCung](
-	[id] [char](10) NOT NULL,
-	[ten] [nvarchar](50) NOT NULL,
-	[hang] [nvarchar](50) NOT NULL,
-	[dungluong] [nchar](10) NOT NULL,
-	[chuan] [nchar](10) NOT NULL,
-	[tocdodoc] [nchar](10) NOT NULL,
-	[tocdoghi] [nchar](10) NOT NULL,
-	[loai] [nchar](10) NOT NULL,
-	[baohanh] [int],
-	[gia] [int],
-	[soluong] [int]
+CREATE TABLE [Manufacturiers]
+(
+	[id][nchar](10) NOT NULL,
+	[name][nvarchar](50) NOT NULL,
+	CONSTRAINT [PK_Manufacturiers] PRIMARY KEY ([id],[name])
 )
 GO
 
--- Tạo cấu trúc bảng tblRAM --
-CREATE TABLE [tblRAM](
-	[id] [char](10) NOT NULL,
-	[ten] [nvarchar](50) NOT NULL,
-	[hang] [nvarchar](50) NOT NULL,
-	[kieu] [char](8) NOT NULL,
-	[dungluong] [nchar](8) NOT NULL,
-	[bus] [nchar](10) NOT NULL,
-	[baohanh] [int],
-	[gia] [int],
-	[soluong] [int]
+CREATE TABLE [RAMTypes]
+(
+	[id][nchar](10) NOT NULL,
+	[type][nvarchar](50) NOT NULL,
+	CONSTRAINT [PK_RAMTypes] PRIMARY KEY ([id])
 )
 GO
 
--- Tạo cấu trúc bảng tblMain --
-CREATE TABLE [tblMain](
-	[id] [char](10) NOT NULL,
-	[ten] [nvarchar](50) NOT NULL,
-	[hang] [nvarchar](50) NOT NULL,
-	[kieu] [char](8) NOT NULL,
-	[socket] [nchar](10) NOT NULL,
-	[chipset] [nchar](10) NOT NULL,
-	[sokheram] [int] NOT NULL,
-	[ramtoida] [nchar](10) NOT NULL,
-	[loairam] [nvarchar](50) NOT NULL,
-	[vgaonboard] [nvarchar](50) NOT NULL,
-	[audio] [nvarchar](50) NOT NULL,
-	[baohanh] [int],
-	[gia] [int],
-	[soluong] [int]
+CREATE TABLE [Sockets]
+(
+	[id][nchar](10) NOT NULL,
+	[type][nvarchar](50) NOT NULL,
+	CONSTRAINT [PK_Sockets] PRIMARY KEY ([id])
 )
 GO
 
--- Tạo cấu trúc bảng tblSocket --
-CREATE TABLE [tblSocket](
-	[id] [nchar](10),
-	[loai] [nchar](10),
-	CONSTRAINT [PK_Socket] PRIMARY KEY ([id],[loai])
+CREATE TABLE [CPUs]
+(
+	[id][nchar](10) NOT NULL,
+	[name][nvarchar](50),
+	[manufacturier][nchar](10) NOT NULL,
+	[corename][nvarchar](50),
+	[core][int],
+	[thread][int],
+	[socket][nchar](10) NOT NULL,
+	[price][int],
+	[quantity][int],
+	CONSTRAINT [PK_CPUs] PRIMARY KEY ([id]),
+	CONSTRAINT [FK_CPUs_Socket] FOREIGN KEY ([socket]) REFERENCES [Sockets]([id])
 )
 GO
 
--- Tạo cấu trúc bảng tblLoaiRAM --
-CREATE TABLE [tblLoaiRAM](
-	[id] [nchar](10),
-	[loai] [nchar](10),
-	CONSTRAINT [PK_LoaiRAM] PRIMARY KEY ([id],[loai])
+CREATE TABLE [RAMs]
+(
+	[id][nchar](10) NOT NULL,
+	[name][nvarchar](50),
+	[manufacturier][nchar](10) NOT NULL,
+	[bus][int],
+	[capacity][nchar](10) NOT NULL,
+	[price][int],
+	[quantity][int],
+	CONSTRAINT [PK_RAMs] PRIMARY KEY ([id])
 )
 GO
 
--- Sửa đổi cấu trúc bảng, thêm các thuộc tính Khóa Chính cho các bảng --
-ALTER TABLE [tblCPU] ADD CONSTRAINT [PK_CPU] PRIMARY KEY ([id])
-ALTER TABLE [tblRAM] ADD CONSTRAINT [PK_RAM] PRIMARY KEY ([id])
-ALTER TABLE [tblMain] ADD CONSTRAINT [PK_Main] PRIMARY KEY ([id])
-ALTER TABLE [tblOCung] ADD CONSTRAINT [PK_OCung] PRIMARY KEY ([id])
+CREATE TABLE [Cases]
+(
+	[id][nchar](10) NOT NULL,
+	[name][nvarchar](50),
+	[manufacturier][nchar](10) NOT NULL,
+	[color][nchar](10),
+	[price][int],
+	[quantity][int],
+	CONSTRAINT [PK_Cases] PRIMARY KEY ([id])
+)
+GO
+
+CREATE TABLE [Disks]
+(
+	[id][nchar](10) NOT NULL,
+	[name][nvarchar](50),
+	[manufacturier][nchar](10) NOT NULL,
+	[type][nchar](10) NOT NULL,
+	[capacity][nchar](10) NOT NULL,
+	[connect][nchar](10),
+	[price][int],
+	[quantity][int],
+	CONSTRAINT [PK_Disks] PRIMARY KEY ([id])
+)
+GO
+
+CREATE TABLE [Mains]
+(
+	[id][nchar](10) NOT NULL,
+	[name][nvarchar](50),
+	[manufacturier][nchar](10) NOT NULL,
+	[socket][nchar](10) NOT NULL,
+	[chipset][nvarchar](50) NOT NULL,
+	[type][nchar](10) NOT NULL,
+	[ramchannel][int],
+	[price][int],
+	[quantity][int],
+	CONSTRAINT [PK_Mains] PRIMARY KEY ([id]),
+	CONSTRAINT [FK_Mains_Socket] FOREIGN KEY ([socket]) REFERENCES [Sockets]([id])
+)
+GO
+
+CREATE TABLE [PSUs]
+(
+	[id][nchar](10) NOT NULL,
+	[name][nvarchar](50),
+	[manufacturier][nchar](10) NOT NULL,
+	[performance][int],
+	[price][int],
+	[quantity][int],
+	CONSTRAINT [PK_PSUs] PRIMARY KEY ([id])
+)
+GO
+
+CREATE TABLE [VGAs]
+(
+	[id][nchar](10) NOT NULL,
+	[name][nvarchar](50),
+	[manufacturier][nchar](10) NOT NULL,
+	[monitor][int],
+	[radiator][nvarchar](50),
+	[price][int],
+	[quantity][int],
+	CONSTRAINT [PK_VGAs] PRIMARY KEY ([id])
+)
+GO
+
+CREATE TABLE [CPUsupportRAMs]
+(
+	[idCPU][nchar](10) NOT NULL,
+	[idRAM][nchar](10) NOT NULL,
+	CONSTRAINT [PK_CPU_RAMs] PRIMARY KEY ([idCPU],[idRAM])
+)
 GO
 
 /*-----------------------------------------------------------------------------------/
